5828a6b38021885288c1985cbd9341e8f34240a2
Karmegam <karmegam.seerangan@syncfusion.com>
2026-09-10 14:48:41 +0530

    Log details before and after _module.DisposeAsync() and recapture both versions Label or sequence the log output and show the second, circuit-related disposal

--- NET 11 Application/BlazorJSObjectReferenceDisposeAsyncSample/Program.cs
+++ NET 11 Application/BlazorJSObjectReferenceDisposeAsyncSample/Program.cs
@@ -4,8 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents(o =>
     {
         o.DisconnectedCircuitRetentionPeriod = TimeSpan.FromSeconds(5);
--- Upgrade NET 10 to NET 11 Application/NET10toNET11UpgradeTest/Program.cs
+++ Upgrade NET 10 to NET 11 Application/NET10toNET11UpgradeTest/Program.cs
@@ -4,7 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents(o =>
+    {
+        o.DisconnectedCircuitRetentionPeriod = TimeSpan.FromSeconds(5);
+    });
 
 var app = builder.Build();
 
